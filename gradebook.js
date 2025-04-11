90e6e94392f6205cda68d0339139d44aba211c7c
Gabe B. <gabrown5@my.waketech.edu>
2025-04-11 12:41:22 -0400

    gradebook.js
    create javascript file to add functionality to Gabe_gradebook.html

---
+++ gradebook.js
@@ -0,0 +1,17 @@
+// TODO Fetch data from the PostgreSQL database
+function fetchGradeData() {
+  // Function to query PostgreSQL db and return grade data
+  console.log("Fetching grade data...");
+}
+
+// TODO: Populate the table with grade data
+function populateGradebook(data) {
+  // Function populates table with fetched grade data
+  console.log("Populating gradebook with data:", data);
+}
+
+// TODO REMOVE THIS
+// Call stubs to demonstrate workflow
+const gradeData = fetchGradeData();
+populateGradebook(gradeData);
+// END REMOVE
